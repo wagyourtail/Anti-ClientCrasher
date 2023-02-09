954831717955e138214991ade65d2c23f8ddce1c
Wagyourtail <wagyourtail@wagyourtail.xyz>
2023-02-09 01:33:48 -0700

    Initial commit

---
+++ forge/src/main/java/xyz/wagyourtail/acc/forge/ACC.java
@@ -0,0 +1,2 @@
+package xyz.wagyourtail.acc.forge;public class ACC {
+}
---
+++ src/main/java/xyz/wagyourtail/acc/mixin/MixinEntitySectionStorage.java
@@ -0,0 +1,13 @@
+package xyz.wagyourtail.acc.mixin;
+
+import net.minecraft.client.multiplayer.ClientPacketListener;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+
+@Mixin(ClientPacketListener.class)
+public class ClientPacketListenerMixin {
+
+//    @Inject(at = @At("HEAD"), method = "onO")
+
+}
